9bbf865ab0a04664a184bcf8353b4945d4b0b086
Alexandre Pellegrino <alesirius@gmail.com>
2020-04-11 01:47:46 -0300

    added functions to list, create and delete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, [repositories]);
+
   async function handleAddRepository() {
-    // TODO
+    api.post("repositories", {
+      title: "Novo Repositorio",
+      url:
+        "https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-nodejs",
+      techs: [
+        "Node",
+        "React",
+        "React Native"
+      ]  
+    });
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`repositories/${id}`)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
